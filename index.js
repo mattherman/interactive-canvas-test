087c5e3897ab568acfacec71f3b3c73c743c40f3
Matthew Herman <mattherman11@gmail.com>
2024-08-04 20:35:43 -0500

    Scaled drawing working

--- index.js
+++ index.js
@@ -1,50 +1,106 @@
 let canvas;
 let ctx;
+let coords;
 
-let start = null;
+let drawing = false;
+let square = null;
+let scale = 1;
 
 function init() {
+    coords = document.getElementById("coords");
     canvas = document.getElementById("image-canvas");
     ctx = canvas.getContext("2d");
 
     canvas.addEventListener("mousedown", function(event) {
         console.log("mousedown", event);
-        start = getRelativeCoordinates(event, canvas);
+        ctx.restore();
+        const start = getRelativeCoordinates(event, canvas);
+        square = { start, end: start };
+        drawing = true;
     });
 
     canvas.addEventListener("mouseup", function(event) {
+        if (!drawing) return;
+
         console.log("mouseup", event);
-        let end = getRelativeCoordinates(event, canvas);
-
-        strokeRect(start, end);
-
-        start = null;
-
+        square.end = getRelativeCoordinates(event, canvas);
+        draw();
+        drawing = false;
     });
 
     canvas.addEventListener("mousemove", function(event) {
-        if (start === null) return;
+        updateCoordinates(event);
+
+        if (!drawing) return;
 
         console.log("mousemove", event);
-
-        clear();
-
-        let end = getRelativeCoordinates(event, canvas);
-        strokeRect(start, end);
+        square.end = getRelativeCoordinates(event, canvas);
+        draw();
     });
 
     document.addEventListener("keydown", function(event) {
         console.log("keypress", event);
-        if (event.key == "Escape") {
+        if (event.key === "Escape") {
             clear();
         }
+        else if (event.key === "ArrowUp") {
+            ctx.translate(0, -10);
+            draw();
+        }
+        else if (event.key === "ArrowDown") {
+            ctx.translate(0, 10);
+            draw();
+        }
+        else if (event.key === "ArrowLeft") {
+            ctx.translate(-10, 0);
+            draw();
+        }
+        else if (event.key === "ArrowRight") {
+            ctx.translate(10, 0);
+            draw();
+        }
     });
+
+    draw();
 }
 
-function getRelativeCoordinates(event, element) {
-    const rect = element.getBoundingClientRect();
-    const x = event.x - rect.left;
-    const y = event.y - rect.top;
+function updateCoordinates(mouseEvent) {
+    const mouseCoords = { x: mouseEvent.x, y: mouseEvent.y };
+    const canvasCoords = { x: mouseCoords.x - canvas.offsetLeft, y: mouseCoords.y - canvas.offsetTop };
+    const scaledCoords = { x: canvasCoords.x / scale, y: canvasCoords.y / scale };
+    coords.innerText =
+        "Global: (" + mouseCoords.x + ", " + mouseCoords.y + "); " + 
+        "Canvas: (" + canvasCoords.x + ", " + canvasCoords.y + ")" + "; " +
+        "Scaled: (" + scaledCoords.x + ", " + scaledCoords.y + ")";
+}
+
+function draw() {
+    clear();
+    strokeRect({ x: 5, y: 5 }, { x: canvas.width - 5, y: canvas.height - 5 });
+    if (square) {
+        strokeRect(square.start, square.end);
+    }
+}
+
+function zoomIn() {
+    scale = scale * 2;
+    ctx.scale(2, 2);
+    draw();
+    console.log("zoomIn");
+}
+
+function zoomOut() {
+    scale = scale * 0.5;
+    ctx.scale(0.5, 0.5);
+    draw();
+    console.log("zoomOut");
+}
+
+function getRelativeCoordinates(event) {
+    const eventX = event.x - canvas.offsetLeft;
+    const eventY = event.y - canvas.offsetTop;
+    const x = eventX / scale;
+    const y = eventY / scale;
     return { x, y };
 }
 
